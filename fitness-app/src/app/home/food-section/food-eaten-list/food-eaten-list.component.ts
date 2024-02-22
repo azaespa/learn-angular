9dd31c220455efd57b53a929b721f03347be0df7
azaespa <pauljamesazanes@gmail.com>
2024-02-22 13:13:26 +0800

    show the list of food eaten from food eaten service

--- fitness-app/src/app/home/food-section/food-eaten-list/food-eaten-list.component.ts
+++ fitness-app/src/app/home/food-section/food-eaten-list/food-eaten-list.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Food } from '../../../food';
+import { FoodEatenService } from '../../../food-eaten.service';
 import { RouterModule } from '@angular/router';
+import { FoodEatenComponent } from './food-eaten/food-eaten.component';
 
 @Component({
   selector: 'app-food-eaten-list',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FoodEatenComponent],
   templateUrl: './food-eaten-list.component.html',
   styleUrl: './food-eaten-list.component.css',
 })
-export class FoodEatenListComponent {}
+export class FoodEatenListComponent {
+  foodEatenList: Food[] = [];
+
+  foodEatenService: FoodEatenService = inject(FoodEatenService);
+
+  constructor() {
+    this.foodEatenList = this.foodEatenService.getFoodEatenList();
+  }
+}
--- fitness-app/src/app/home/food-section/food-eaten-list/food-eaten/food-eaten.component.ts
+++ fitness-app/src/app/home/food-section/food-eaten-list/food-eaten/food-eaten.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Food } from '../../../../food';
 
 @Component({
   selector: 'app-food-eaten',
   standalone: true,
   imports: [],
   templateUrl: './food-eaten.component.html',
-  styleUrl: './food-eaten.component.css'
+  styleUrl: './food-eaten.component.css',
 })
 export class FoodEatenComponent {
-
+  @Input() foodEaten!: Food;
 }
